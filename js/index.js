d35e62da9594572dc36968b2cbaed85ff151f945
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-08-10 12:09:35 +0100

    fixed the projects, finished product but could do with a few more touches, 1 more push left

--- js/index.js
+++ js/index.js
@@ -7,19 +7,21 @@ fetch(`data/data.json`)
     inputData(data);
   });
 function inputData(data) {
-  const biscuitTarget = document.querySelector(`.projects`);
+  const dataTarget = document.querySelector(`.projects`);
 
   data.projects.forEach((item, index) => {
-    biscuitTarget.innerHTML += `<div class="project"> <br />
+    dataTarget.insertAdjacentHTML(
+      "afterend",
+      `<div class="project"> <br />
     <img src="${item.img}" alt="a screenshot of the project" class="projectImage"/> <br /> 
       <div class="info"> <br /> 
       <h1 class="projectTitle">${item.name}</h1> <br /> 
       <p class="info">${item.desc}</p> <br /> 
-      <button id="myBtn${index}">See more...</button> <br /> 
+      <button class="modalButton" id="myBtn${index}">See more...</button> <br /> 
       </div> <br /> 
       <div id="myModal${index}" class="modal"> <br /> 
-      <span class="close">&times;</span> <br /> 
-      <div class="modal-content${index}"> <br /> 
+      <div class="modalContent" id="modal-content${index}"> <br /> 
+      <span class="close"></span> <br /> 
       <img
     src="${item.img2}"
     alt="another image of the project"
@@ -27,32 +29,24 @@ function inputData(data) {
     /> <br /> <p class="moreInfoTarget">${item.desc2}</p> <br /> 
     </div> <br /> 
     </div> <br /> 
-    </div> <br />`;
+    </div> <br />`
+    );
 
     // Get the modal
-    var modal = document.getElementById(`myModal${index}`);
-
+    let modal = document.querySelector(`#myModal${index}`);
     // Get the button that opens the modal
-    var btn = document.getElementById(`myBtn${index}`);
-
+    let btn = document.getElementById(`myBtn${index}`);
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+    let span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal
-    btn.onclick = function () {
+    btn.addEventListener("click", () => {
       modal.style.display = "block";
-    };
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
+    });
+    span.addEventListener("click", () => {
       modal.style.display = "none";
-    };
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    };
+    });
+    modal.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
   });
 }
